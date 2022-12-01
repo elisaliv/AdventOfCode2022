7964eee3dfbb13d7e61dc5c9bb0ce233f8561b2d
Elisa <49102376+elisaliv@users.noreply.github.com>
2022-12-01 21:41:20 +0000

    Day 1 - part two

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import fileinput
 
 def day_01():
   max_calories = 0
+  second_max = 0
+  third_max = 0
   calories = 0
 
   with fileinput.input(files=('calories.txt')) as f:
@@ -10,13 +12,24 @@ def day_01():
       if line != '\n':
         calories += int(line)
       else:
-        if calories > max_calories:
+        if calories >= third_max and calories < second_max:
+          third_max = calories
+        if calories >= second_max and calories < max_calories:
+          third_max = second_max
+          second_max = calories
+        if calories >= max_calories:
+          third_max = second_max
+          second_max = max_calories
           max_calories = calories
+
         print('Calories carried by elf:', calories)
         print('Max calories so far:', max_calories)
+        print('Second max:', second_max)
+        print('Third max:', third_max)
         calories = 0
 
   print('Max calories:', max_calories)
+  print('Total top three calories:', max_calories + second_max + third_max)
 
 
 def main():
